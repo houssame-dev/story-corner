50dd390cfe9d07b614a63d2b75eb86f9876b32a6
houssame errjem <errjem2@gmail.com>
2023-10-22 18:52:09 +0100

    commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://story-corner.onrender.com/" });
+const api = axios.create({ BASE_URL: "https://story-corner.onrender.com/" });
 
 api.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
