9567f483923ad864c9f2e6832c8c97e5b4b8a99f
houssame errjem <errjem2@gmail.com>
2023-10-22 19:03:41 +0100

    commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,20 +1,23 @@
 import axios from "axios";
 
-const api = axios.create({ BASE_URL: "https://story-corner.onrender.com/" });
+const api = axios.create({ PORT: "https://story-corner.onrender.com/" });
 
 api.interceptors.request.use((req) => {
-    if (localStorage.getItem("profile")) {
-        const profile = JSON.parse(localStorage.getItem("profile"));
-        req.headers.Authorization = `Bearer ${profile.token}`;
-    }
-    return req;
+  if (localStorage.getItem("profile")) {
+    const profile = JSON.parse(localStorage.getItem("profile"));
+    req.headers.Authorization = `Bearer ${profile.token}`;
+  }
+  return req;
 });
 
 export const fetchStories = async () => api.get("/stories");
 export const createStory = async (story) => api.post("/stories", story);
-export const updateStory = async (id, story) => api.patch(`${"/stories"}/${id}`, story);
+export const updateStory = async (id, story) =>
+  api.patch(`${"/stories"}/${id}`, story);
 export const deleteStory = async (id) => api.delete(`${"/stories"}/${id}`);
-export const likeStory = async (id) => api.patch(`${"/stories"}/${id}/likeStory`);
+export const likeStory = async (id) =>
+  api.patch(`${"/stories"}/${id}/likeStory`);
 
 export const login = async (formValues) => api.post("/user/login", formValues);
-export const signup = async (formValues) => api.post("/user/signup", formValues);
+export const signup = async (formValues) =>
+  api.post("/user/signup", formValues);
