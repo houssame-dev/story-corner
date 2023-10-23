30629168f124e4872f95e913e687926bcb0cbc4e
houssame errjem <errjem2@gmail.com>
2023-10-23 01:11:20 +0100

    changes

--- client/src/actions/authentication.js
+++ client/src/actions/authentication.js
@@ -1,26 +1,62 @@
-import * as api from "../api";
+import Swal from 'sweetalert2'; // Import SweetAlert2
+
+import * as api from '../api';
 import {
-    AUTHENTICATION
-} from "../constants/actionTypes";
+  AUTHENTICATION
+} from '../constants/actionTypes';
 
 const signup = (formValues, navigate) => async dispatch => {
-    try {
-        const { data } = await api.signup(formValues);
-        dispatch({ type: AUTHENTICATION, data: data });
-        navigate("/");
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const { data } = await api.signup(formValues);
+    dispatch({ type: AUTHENTICATION, data: data });
+
+    // Show a success message with SweetAlert2
+    Swal.fire({
+      title: 'Success',
+      text: 'Your account was created successfully',
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+
+    // Show an error message with SweetAlert2
+    Swal.fire({
+      title: 'Error',
+      text: 'Failed to signup',
+      icon: 'error',
+    });
+  }
 };
 
 const login = (formValues, navigate) => async dispatch => {
-    try {
-        const { data } = await api.login(formValues);
-        dispatch({ type: AUTHENTICATION, data: data });
-        navigate("/");
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const { data } = await api.login(formValues);
+    dispatch({ type: AUTHENTICATION, data: data });
+
+    // Show a success message with SweetAlert2
+    Swal.fire({
+      title: 'Success',
+      text: 'You have successfully logged in',
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+
+    // Show an error message with SweetAlert2
+    Swal.fire({
+      title: 'Error',
+      text: 'Login failed',
+      icon: 'error',
+    });
+  }
 };
 
 export { signup, login };
--- client/src/actions/stories.js
+++ client/src/actions/stories.js
@@ -1,51 +1,118 @@
+import Swal from "sweetalert2"; // Import SweetAlert2
+
 import * as api from "../api";
 import {
-    FETCH_ALL_STORIES,
-    CREATE_STORY,
-    UPDATE_STORY,
-    DELETE_STORY
+  FETCH_ALL_STORIES,
+  CREATE_STORY,
+  UPDATE_STORY,
+  DELETE_STORY,
 } from "../constants/actionTypes";
+
 export const getStories = () => async (dispatch) => {
-    try {
-        const { data } = await api.fetchStories();
-        dispatch({ type: FETCH_ALL_STORIES, payload: data });
-    } catch (error) {
-        console.log(error.message)
-    }
+  try {
+    const { data } = await api.fetchStories();
+    dispatch({ type: FETCH_ALL_STORIES, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const createStory = (story) => async (dispatch) => {
-    try {
-        const { data } = await api.createStory(story);
-        dispatch({ type: CREATE_STORY, payload: data });
-    } catch (error) {
-        console.log(error.message)
-    }
+  try {
+    const { data } = await api.createStory(story);
+    dispatch({ type: CREATE_STORY, payload: data });
+
+    // Show a success message with SweetAlert2
+    Swal.fire({
+      title: "Success",
+      text: "Story created successfully",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    // Show an error message with SweetAlert2
+    Swal.fire({
+      title: "Error",
+      text: "Failed to create the story",
+      icon: "error",
+    });
+  }
 };
 
 export const updateStory = (id, story) => async (dispatch) => {
-    try {
-        const { data } = await api.updateStory(id, story);
-        dispatch({ type: UPDATE_STORY, payload: data });
-    } catch (error) {
-        console.log(error.message)
-    }
+  try {
+    const { data } = await api.updateStory(id, story);
+    dispatch({ type: UPDATE_STORY, payload: data });
+
+    // Show a success message with SweetAlert2
+    Swal.fire({
+      title: "Success",
+      text: "Story updated successfully",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    // Show an error message with SweetAlert2
+    Swal.fire({
+      title: "Error",
+      text: "Failed to update the story",
+      icon: "error",
+    });
+  }
 };
 
 export const deleteStory = (id, story) => async (dispatch) => {
-    try {
-        await api.deleteStory(id);
-        dispatch({ type: DELETE_STORY, payload: id });
-    } catch (error) {
-        console.log(error.message)
-    }
+  try {
+    await api.deleteStory(id);
+    dispatch({ type: DELETE_STORY, payload: id });
+
+    // Show a success message with SweetAlert2
+    Swal.fire({
+      title: "Success",
+      text: "Story deleted successfully",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    // Show an error message with SweetAlert2
+    Swal.fire({
+      title: "Error",
+      text: "Failed to delete the story",
+      icon: "error",
+    });
+  }
 };
 
 export const likeStory = (id) => async (dispatch) => {
-    try {
-        const { data } = await api.likeStory(id);
-        dispatch({ type: UPDATE_STORY, payload: data });
-    } catch (error) {
-        console.log(error.message)
-    }
+  try {
+    const { data } = await api.likeStory(id);
+    dispatch({ type: UPDATE_STORY, payload: data });
+
+    // Show a success message with SweetAlert2
+    Swal.fire({
+      title: "Success",
+      text: "Story liked successfully",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    // Show an error message with SweetAlert2
+    Swal.fire({
+      title: "Error",
+      text: "Failed to like the story",
+      icon: "error",
+    });
+  }
 };
